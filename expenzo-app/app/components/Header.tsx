586d10fe02310ab0c8548e9927fbffd6cd79423a
G.S.Chandhru <gschandhru10@gmail.com>
2025-07-31 23:41:04 +0530

    feat(header) : Basic header added for app

---
+++ expenzo-app/app/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <header className='w-screen h-13 bg-amber-300 flex justify-between items-center'>
+            <div className='h-full bg-amber-700 w-25  grid place-items-center'>Ex</div>
+            <div className='mr-2.5'>User</div>
+        </header>
+    )
+}
+export default Header
